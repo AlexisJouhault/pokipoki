5fb94a59e24c35fd8438a8f732b15b78afd4f6ae
AlexisEveryStory <alexis.jouhault@gmail.com>
2016-06-19 21:13:11 -0700

    wraning removed

--- app/src/main/java/pokemeows/com/pokipoki/LoginActivity.java
+++ app/src/main/java/pokemeows/com/pokipoki/LoginActivity.java
@@ -3,14 +3,11 @@ package pokemeows.com.pokipoki;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import org.greenrobot.eventbus.EventBus;
-
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
     }
 }
