633c146a141e366cbbbf6930965f35de07ad7410
WOMYNxH4CKERS <csusm.hackathon@gmail.com>
2016-07-06 20:38:34 -0700

    comments added

--- app/src/main/java/com/pokemeows/pokipoki/Tools/Database/Models/Card.java
+++ app/src/main/java/com/pokemeows/pokipoki/Tools/Database/Models/Card.java
@@ -4,12 +4,18 @@ package com.pokemeows.pokipoki.tools.database.models;
  * Created by alexisjouhault on 7/4/16.
  * ~~PokiPoki project~~
  */
+
+/***
+ *  Every single card that is made will be based on this model
+ *  Long id: is for the database
+ */
 public class Card {
 
     private String name;
     private Long id;
     private int rarity;
     private int type;
+    private int imageID;
 
     enum RARITY {
         COMMON(0),
